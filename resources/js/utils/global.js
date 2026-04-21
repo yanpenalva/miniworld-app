51e1d05038b7de15d72de1d25ed3bf081666c0d0
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2026-04-21 13:13:05 -0300

    feat: update environment configuration and global version handling

--- resources/js/utils/global.js
+++ resources/js/utils/global.js
@@ -1,13 +1,5 @@
 const useGlobal = () => {
-  const versions = new Map([
-    ['local', '0.0.0'],
-    ['development', '0.0.0'],
-    ['staging', '0.0.0'],
-    ['production', '0.0.0'],
-  ]);
-
-  const env = import.meta.env.VITE_APP_ENV;
-  const version = versions.get(env) ?? '0.0.0';
+  const version = import.meta.env.VITE_APP_VERSION ?? '0.0.0';
 
   return { appVersion: `v ${version}` };
 };
